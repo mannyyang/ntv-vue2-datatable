94aa179e0f945819b1ff02970d93cbe761cddbf8
Manuel Yang <myang@nativo.com>
2018-05-10 13:48:30 -0700

    Remove i18n code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ Datatable.install = (Vue, options) => {
   const locale = options && options.locale || {}
   
   // this might be the simplest i18n solution
-  Vue.prototype.$i18nForDatatable = srcTxt => locale[srcTxt] || srcTxt
+  // Vue.prototype.$i18nForDatatable = srcTxt => locale[srcTxt] || srcTxt
 
   Vue.component('Datatable', Datatable)
 }
